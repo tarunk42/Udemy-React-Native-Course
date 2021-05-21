5c32dacdc9f7a8895df8b48c44e8949b1ce284a0
tarun kashyap <tarunsinghkashyap@gmail.com>
2021-05-21 13:01:01 +0530

    Adding 3rd project

---
+++ Third Project/instances/track-server-working-upto-error handling/src/models/User.js
@@ -0,0 +1,16 @@
+// Designing a User Schema                              Step:4
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model('User', userSchema);
